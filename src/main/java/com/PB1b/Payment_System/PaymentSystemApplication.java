a649b6425ab2b4d073afaff5f346ed628d315e4a
Saurabhdarekar <saurabhdarekar724@gmail.com>
2022-08-23 13:48:07 +0530

    Initialized File

---
+++ src/main/java/com/PB1b/Payment_System/PaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.PB1b.Payment_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PB1b/Payment_System/PaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PB1b.Payment_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
